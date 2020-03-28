4933d2d49ac5eb8ef718da7e04da63ee2df06163
Melissa Manousos <mmanousos@gmail.com>
2020-03-27 19:43:57 -0700

    Revised docker-compose file and stripped down producer

--- consumer/src/value_consumer.rb
+++ consumer/src/value_consumer.rb
@@ -2,10 +2,13 @@ require 'avro_turf/messaging'
 require 'kafka'
 
 # You need to pass the URL of your Schema Registry.
-avro = AvroTurf::Messaging.new(registry_url: "http://schema-registry:28081/", schemas_path: "value-comsumer/src/app/schemas")
+# avro = AvroTurf::Messaging.new(registry_url: "http://localhost:8081/", schemas_path: "consumer/src/schemas")
+# avro = AvroTurf::Messaging.new(registry_url: "http://schema-registry:28081")
 
 # These commands are all from kafka-ruby for setting up a consumer
-kafka = Kafka.new(["localhost:9092"])
+# kafka = Kafka.new(["localhost:9092"])
+# kafka = Kafka.new(["broker:29092"])
+kafka = Kafka.new(["broker.confluent_kafka:29092", "broker:29092", "localhost:9092"])
 consumer = kafka.consumer(group_id: "number-consumer")
 consumer.subscribe("number")
 
@@ -17,7 +20,10 @@ consumer.each_message do |message|
   # the schema in an incompatible way. You can leave out the argument, in
   # which case you'll just get whatever data the producer encoded out, with
   # no schema compatibility guarantees.
-  event_message = avro.decode(message.value, schema_name: "values")
-  puts event_message #=> { "title" => "hello, world" }
+ 
+  # event_message = avro.decode(message.value, schema_name: "values")
+  # event_message = avro.decode(message.value)
+puts message
+  # puts event_message #=> { "title" => "hello, world" }
   puts ''
 end
--- producer/src/value_producer.rb
+++ producer/src/value_producer.rb
@@ -1,13 +1,15 @@
+require 'avro_turf'
 require 'avro_turf/messaging'
 require 'date'
 require 'kafka'
 require 'timeloop'
 
 # connects to the local Kafka instance
-kafka = Kafka.new(["localhost:9092"])
+# kafka = Kafka.new(["localhost:9092"])
+kafka = Kafka.new(["broker:29092"])
 
 # You need to pass the path to the avro schema file
-avro = AvroTurf::Messaging.new(registry_url: "http://schema-registry:28081", schemas_path: "value-producer/src/app/schemas")
+# avro = AvroTurf::Messaging.new(registry_url: "http://localhost:8081", schemas_path:"producer/src/schemas/")
 
 # uses the ruby-kafka library to set up an asyncronous producer
 producer = kafka.async_producer
@@ -23,12 +25,14 @@ end
 # uses the Timeloop library to set up an interval
 Timeloop.every 2.seconds do
   msg = generate_object
+
 # Encoding data has the side effect of registering the schema. This only
 # happens the first time a schema is used with the instance of `AvroTurf`.
-  data = avro.encode(msg, schema_name: "values")
+  # data = avro.encode(msg, schema_name: "numbers")
 
-  producer.produce(data, topic: "number") # `.produce` is a method from the ruby-kafka library
-  producer.deliver_messages             # `.deliver_messages` is another method from the ruby-kafka library
+  producer.produce(msg, topic: "number") # `.produce` is a method from the ruby-kafka library
+  # producer.produce(data, topic: "number") # `.produce` is a method from the ruby-kafka library
+  producer.deliver_messages               # `.deliver_messages` is another method from the ruby-kafka library
   puts msg
 end
 
